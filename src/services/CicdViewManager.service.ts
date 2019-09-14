2a9300de05b620b653662c5f4c242905b034a7f7
sourav <sourav@finzy.com>
2019-09-14 13:37:31 +0530

    #006 | Sourav | Controlling Di-LED for build status.

--- src/services/CicdViewManager.service.ts
+++ src/services/CicdViewManager.service.ts
@@ -12,18 +12,37 @@ export class CicdViewManagerService {
     async updateCicdView(): Promise<void> {
         const components = ComponentModel.getAllComponents();
         for (let index = 0; index < components.length; index++) {
-            const buildResult = await this.componentBuildService.getBuildDetailsFor(components[index]);
+            let buildResult;
+            try {
+                buildResult = await this.componentBuildService.getBuildDetailsFor(components[index]);
+            } catch (e) {
+                // when failed to fetch CICD data
+                this.stopShowingStatus();
+                return;
+            }
             if (buildResult.lastBuildStatus === 'Failure') {
-                this.raspberryPiDriver.changeBuildStatusForSuccess(ViewCode.OFF);
-                this.raspberryPiDriver.changeBuildStatusForFail(ViewCode.ON);
-                console.log(`${components[index]} build failed`);
+                this.showBuildFailed();
                 return;
             } else {
                 continue;
             }
         }
+        this.showBuildSuccess();
+    }
+
+    private stopShowingStatus(): void {
+        this.raspberryPiDriver.changeBuildStatusForSuccess(ViewCode.OFF);
+        this.raspberryPiDriver.changeBuildStatusForFail(ViewCode.OFF);
+    }
+
+    private showBuildSuccess(): void {
         this.raspberryPiDriver.changeBuildStatusForSuccess(ViewCode.ON);
         this.raspberryPiDriver.changeBuildStatusForFail(ViewCode.OFF);
+    }
+
+    private showBuildFailed(): void {
+        this.raspberryPiDriver.changeBuildStatusForSuccess(ViewCode.OFF);
+        this.raspberryPiDriver.changeBuildStatusForFail(ViewCode.ON);
     }
 }
 
